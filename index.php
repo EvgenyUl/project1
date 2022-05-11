51b24bf0c035e6be44886592e89979ee3527fee0
Evgeny <evgenyulchiev@gmail.com>
2022-05-11 09:29:59 +0300

    This is the commit title
    And in this place there are more information about commit.
    
     Please enter the commit message for your changes. Lines starting
     with '#' will be ignored, and an empty message aborts the commit.
    
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
           modified:   composer.json
           modified:   index.php

--- index.php
+++ index.php
@@ -3,9 +3,7 @@
 error_reporting(-1);
 ini_set('display_errors', 1);
 
-ini_get('log_errors');
-ini_get('errors_display');
-
 require __DIR__ . '/vendor/autoload.php';
 
 echo ((new Dudom\Foo)->getClass());
+new Dudom\App;
